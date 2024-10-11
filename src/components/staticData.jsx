9d3e3662e9a15f62cc6ef8ba7e45d29918b0164e
Shenouda Mikhael <Shenouda.Mikhael@gmail.com>
2024-10-11 11:29:09 +0300

    added auth to socket connection

--- src/components/staticData.jsx
+++ src/components/staticData.jsx
@@ -2,7 +2,13 @@ import io from 'socket.io-client';
 
 
 class StaticData {
-    static socket = io('http://192.168.1.18:3000', { transports: ['websocket'] });
+    static socket =  io('http://192.168.1.18:3000', {
+        transports: ['websocket'],
+        // auth: {
+        //     token: 'your_jwt_token_here'  // Replace with the actual JWT token
+        // },
+
+    });
 
 
 }
